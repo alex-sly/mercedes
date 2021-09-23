144dc11ea05c02248409387cb517a4190092cd60
alex-sly <80974804+alex-sly@users.noreply.github.com>
2021-09-23 18:11:07 +0500

    Add modal window & mobile menu

--- js/accordion.js
+++ js/accordion.js
@@ -1,16 +1,16 @@
 const btns = document.querySelectorAll(".feature__link");
 const lists = document.querySelectorAll(".feature-sub");
 
-btns.forEach((btnItem, index) => {
-  btnItem.addEventListener("click", () => {
-    btns.forEach((btnItem) => {
-      btnItem.classList.remove("feature__link_active");
+btns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    btns.forEach((btnItem, idx) => {
+      if (btnItem === btn) {
+        btnItem.classList.toggle("feature__link_active");
+        lists[idx].classList.toggle("hidden");
+      } else {
+        btnItem.classList.remove("feature__link_active");
+        lists[idx].classList.add("hidden");
+      }
     });
-    btnItem.classList.add("feature__link_active");
-
-    lists.forEach((listItem) => {
-      listItem.classList.add("hidden");
-    });
-    lists[index].classList.remove("hidden");
   });
 });
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -6,6 +6,8 @@ newArray.forEach((link) => {
   link.addEventListener("click", (event) => {
     event.preventDefault();
     const ID = event.target.getAttribute("href").substr(1);
+    menuElem.classList.remove("menu-active");
+    burger.classList.remove("humburger-menu-active");
     document.getElementById(ID).scrollIntoView({
       behavior: "smooth",
       block: "start",
